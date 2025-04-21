49f9e9d0f8af7ffec52b117a3992bf9092751544
GhazanfarIjazOcta <ghazanfar@octathorn.com>
2025-04-21 08:45:34 +0500

    setting deployment errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,html}"], // adjust path based on your project structure
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-dark": "#004466",
+        primary: "#006699",
+        "primary-light": "#0099cc",
+        secondary: "#00ccff",
+        accent: "#66ffff",
+        dark: "#001a33",
+        light: "#e6f9ff",
+        "gray-light": "#f0f7fa",
+        gray: "#7a8b99",
+        "text-dark": "#002233",
+        "text-light": "#e6f9ff",
+      },
+      spacing: {
+        xs: "0.25rem",
+        sm: "0.5rem",
+        md: "1rem",
+        lg: "1.5rem",
+        xl: "2.5rem",
+        xxl: "4rem",
+      },
+    },
   },
   plugins: [],
 };
